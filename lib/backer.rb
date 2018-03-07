d45828589646ef868381bde05c8e64b8ea10523f
Marin Cabac <marin.cabac@gmail.com>
2018-03-07 20:32:28 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,15 @@
+class Backer
+  attr_accessor  :name, :backed_projects
+
+  def initialize(name)
+    @name=name
+    @backed_projects =[]
+      end
+
+
+def back_project(proj)
+
+      @backed_projects << proj
+      proj.backers << self
+    end
+end
---
+++ lib/project.rb
@@ -0,0 +1,18 @@
+class Project
+  attr_accessor :title
+
+  def initialize(title)
+    @title=title
+    @backers=[]
+  end
+
+  def add_backer(bkr)
+    @backers<<bkr
+    bkr.backed_projects<<self
+ end
+
+  def backers
+    @backers
+  end
+
+end
