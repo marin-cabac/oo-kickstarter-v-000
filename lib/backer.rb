322c28476d2a24eed5ea095b817636bc4631b1bf
Marin Cabac <marin.cabac@gmail.com>
2018-03-07 17:38:30 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,11 @@
 class Backer
   attr_accessor :backed_projects, :name
-
+@backed_projects = []
   def initialize(name)
     @name=name
-    @backed_projects = []
-  end
+      end
 
+def back_project(proj)
+      @backed_projects << proj
+    end 
 end
